863dcfd9e4fb3740def557cdb7d68d14765a875b
KholisEnka22 <enka.run2206@gmail.com>
2021-01-13 14:24:19 +0700

    Date Time

---
+++ datetime/SystemCurrentMilis.java
@@ -0,0 +1,11 @@
+package datetime;
+
+
+public class SystemCurrentMilis {
+
+    public static void main(String[] args) {
+        long timeNow = System.currentTimeMillis();
+        System.out.println("Waktu sekarang adalah " + timeNow + " milisecond");
+    }
+    
+}
